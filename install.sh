e5d82b2129f2339327b5c91b7295fdf265d49822
LI Qing <geding.lq@alibaba-inc.com>
2019-11-22 18:23:19 +0800

    Install enable_rdfsbase module permanently
    Modify the scripts to make it enabled even after the next OS reboot.

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
+
 sudo mkdir -p "/lib/modules/"`uname -r`"/kernel/drivers/occlum"
 sudo cp enable_rdfsbase.ko "/lib/modules/"`uname -r`"/kernel/drivers/occlum"
-sudo sh -c "cat /etc/modules | grep -Fxq isgx || echo enable_rdfsbase >> /etc/modules"
+
+if [ -e "/etc/modules" ]; then
+    # Ubuntu
+    sudo sh -c "cat /etc/modules | grep -Fxq isgx || echo enable_rdfsbase >> /etc/modules"
+elif [ -d "/etc/modules-load.d" ]; then
+    # CentOS
+    sudo sh -c "echo \"enable_rdfsbase\" > /etc/modules-load.d/enable_rdfsbase.conf"
+fi
 sudo /sbin/depmod
 sudo /sbin/modprobe enable_rdfsbase
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+
 sudo /sbin/modprobe -r enable_rdfsbase
 sudo rm -rf "/lib/modules/"`uname -r`"/kernel/drivers/occlum"
 sudo /sbin/depmod
-sudo /bin/sed -i '/^enable_rdfsbase$/d' /etc/modules
+if [ -e "/etc/modules" ]; then
+    # Ubuntu
+    sudo /bin/sed -i '/^enable_rdfsbase$/d' /etc/modules
+elif [ -d "/etc/modules-load.d" ]; then
+    # CentOS
+    sudo rm -f /etc/modules-load.d/enable_rdfsbase.conf
+fi
