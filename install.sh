04a454f4f59f325240ed957e4add2f2eeecf51ae
LI Qing <geding.lq@antfin.com>
2021-03-01 21:22:43 +0800

    Fix the install script for Ubuntu

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo cp enable_rdfsbase.ko "/lib/modules/"`uname -r`"/kernel/drivers/occlum"
 
 if [ -e "/etc/modules" ]; then
     # Ubuntu
-    sudo sh -c "cat /etc/modules | grep -Fxq isgx || echo enable_rdfsbase >> /etc/modules"
+    sudo sh -c "cat /etc/modules | grep -Fxq enable_rdfsbase || echo enable_rdfsbase >> /etc/modules"
 elif [ -d "/etc/modules-load.d" ]; then
     # CentOS
     sudo sh -c "echo \"enable_rdfsbase\" > /etc/modules-load.d/enable_rdfsbase.conf"
